e92a892d1a1415290a5aa97f478867950341eb0f
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-05-17 14:49:59 +0100

    edited dao to include the users product choices and group them by type

--- app/src/main/java/com/example/skinnote/ProductsEntity.kt
+++ app/src/main/java/com/example/skinnote/ProductsEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 // creates table for spinner skin products
 @Entity(tableName = "products")
-data class ProductsEntity(
+data class ProductsEntry(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,       // product name
     val type: String        // whether its face wash, cleanser etc
--- app/src/main/java/com/example/skinnote/SkinNoteDao.kt
+++ app/src/main/java/com/example/skinnote/SkinNoteDao.kt
@@ -9,11 +9,19 @@ import androidx.room.Query
 @Dao
 interface SkinNoteDao {
 
-    // adds new entry to db
+    // adds new entry to user choice db
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEntry(entry: SkinEntry)
 
     // sorts all entries by time
     @Query("SELECT * FROM skincare_entries ORDER BY timestamp DESC")
     suspend fun getAllEntries(): List<SkinEntry>
+
+    // adds new products to products db (from dialog)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProduct(product: ProductsEntry)
+
+    // groups products of a certain type
+    @Query("SELECT * FROM products WHERE type = :type")
+    suspend fun getProductsByType(type: String): List<ProductsEntry>
 }
