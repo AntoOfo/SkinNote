532441af0d95b6bce6d62be61f281727396530d7
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-05-17 13:28:05 +0100

    db created to link entity & dao

---
+++ app/src/main/java/com/example/skinnote/SkinNoteDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.skinnote
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// connecting entity and dao, 1st entity
+@Database(entities = [SkinEntry::class], version = 1)
+abstract class SkinNoteDatabase : RoomDatabase() {
+
+    // bringing in dao
+    abstract fun skinNoteDao(): SkinNoteDao
+
+    companion object {
+        // single instance to prevent duplicate dbs
+        @Volatile
+        private var INSTANCE: SkinNoteDatabase? = null
+
+        // either returns db or makes it if it doesnt exist
+        fun getDatabase(context: Context): SkinNoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SkinNoteDatabase::class.java,
+                    "SkinNoteDatabase"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
