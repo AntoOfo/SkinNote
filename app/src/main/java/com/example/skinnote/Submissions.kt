273f051cf5c1dbb5d0f7aa4429f246fb6526466e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-05-17 18:56:29 +0100

    setup recyclerview in Submissions activity

--- app/src/main/java/com/example/skinnote/Submissions.kt
+++ app/src/main/java/com/example/skinnote/Submissions.kt
@@ -5,8 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 
 class Submissions : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: SubmissionAdapter
+    private lateinit var dao: SkinNoteDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,19 @@ class Submissions : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        recyclerView = findViewById(R.id.submissionsRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // grab dao from db
+        dao = SkinNoteDatabase.getDatabase(applicationContext).skinNoteDao()
+
+        // get data from db nd shows it
+        lifecycleScope.launch {
+            val entries = dao.getAllEntries()
+            adapter = SubmissionAdapter(entries)
+            recyclerView.adapter = adapter
         }
 
 
